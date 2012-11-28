c4882deebf05a79a7ece9e35db6e09076d409066
Kevin Swiber <kswiber@gmail.com>
2012-11-28 13:55:44 -0500

    Simplifying API.

--- runner.js
+++ runner.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var Runner = function() {};
 
-Runner.prototype.start = function(platform) {
+Runner.prototype.listen = function(platform, port) {
   http.createServer(function(req, res) { 
     req.queryParams = { };
     var env = { request: req, response: res, target: {}, proxy: { pathSuffix: req.url } };
     platform.call(env);
-  }).listen(process.env.PORT || 3000);
+  }).listen((process.env.PORT || port) || 3000);
 };
 
 module.exports = new Runner();
