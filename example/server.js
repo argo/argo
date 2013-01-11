6033c06596daf8def3cc753ece6b8b4ab242e05c
Kevin Swiber <kswiber@gmail.com>
2013-01-11 15:50:15 -0500

    Adding package support for Argo and an an Argo package for OAuth2

--- example/server.js
+++ example/server.js
@@ -1,10 +1,17 @@
 var argo = require('../');
 var cors = require('./cors');
+var oauth2 = require('../argo-oauth2-package');
+
+var oauth = oauth2
+  .configure(require('./oauthOptions'))
+  .support('authorization_code', 'bearer');
 
 var proxy = argo();
+
 var port = process.env.PORT || 3000;
 
 proxy
+  .use(oauth)
   .use(cors)
   .route('/weather/forecasts', require('./forecasts'))
   .listen(port);
