d3b20d51589723ebcea9c7c83cc78beaee009d7d
Kevin Swiber <kswiber@gmail.com>
2013-08-22 11:00:26 -0400

    Cleaning up runner.js before the next refactor.

--- runner.js
+++ runner.js
@@ -5,45 +5,37 @@ var util = require('util');
 
 var Runner = function() {};
 
-//var numCPUs = require('os').cpus().length;
-
 Runner.prototype.listen = function(platform, port) {
-  /*if (cluster.isMaster) {
-    for (var i = 0; i < numCPUs; i++) {
-      cluster.fork();
-    }
+  var app = platform.build();
+  var serverDomain = domain.create();
 
-    cluster.on('exit', function(worker, code, signal) {
-      console.log('LOG: worker ' + worker.process.pid + ' died');
-    });
-  } else*/ {
-    var app = platform.build();
-    var serverDomain = domain.create();
-    serverDomain.run(function domainRunner() {
-      http.createServer(function httpRequestHandler(req, res) {
-        var requestDomain = domain.create();
-        requestDomain.add(req);
-        requestDomain.add(res);
-        // TODO: Remove asterisk.  Fix reporting in dev mode.
-        requestDomain.on('error', function(err) {
-          console.log('ERROR:', err.toString(), err.stack, req.url, err);
+  serverDomain.run(function domainRunner() {
+    http.createServer(function httpRequestHandler(req, res) {
+      var requestDomain = domain.create();
 
-          try {
-            res.writeHead(500);
-            res.end('Internal Server Error');
-            res.on('close', function() {
-              requestDomain.dispose();
-            });
-          } catch (err) {
-            console.log('ERROR: Unable to send 500 Internal Server Error', 
-              err.toString(), err.stack, req.url, err);
+      requestDomain.add(req);
+      requestDomain.add(res);
+
+      requestDomain.on('error', function(err) {
+        console.log('ERROR:', err.toString(), err.stack, req.url, err);
+
+        try {
+          res.writeHead(500);
+          res.end('Internal Server Error');
+          res.on('close', function() {
             requestDomain.dispose();
-          }
-        });
-        app.run(req, res);
-      }).listen(port);
-    });
-  }
+          });
+        } catch (err) {
+          console.log('ERROR: Unable to send 500 Internal Server Error',
+            err.toString(), err.stack, req.url, err);
+          requestDomain.dispose();
+        }
+      });
+
+      app.run(req, res);
+    })
+    .listen(port);
+  });
 };
 
 module.exports = new Runner();
